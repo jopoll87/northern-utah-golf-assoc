6c72d71525b055b7cd74e254d6e472ef89c2f365
Jon Poll <jon.poll@schools.utah.gov>
2024-12-18 14:58:28 -0700

    Fixed styling, added svg logo, and form validation

--- src/components/SignUpModal.tsx
+++ src/components/SignUpModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, ModalHeader, Button, ModalContent, Form, FormField } from 'semantic-ui-react';
+import { Modal, ModalHeader, Button, ModalContent, Form, Input, FormGroup, FormInput } from 'semantic-ui-react';
 
 const SignUpModal = () => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -13,23 +13,46 @@ const SignUpModal = () => {
       <ModalHeader>Sign Up For Norther Utah Golf Association</ModalHeader>
       <ModalContent>
         <Form>
-          <FormField>
-            <label>First Name</label>
-            <input placeholder='First Name'/>
-          </FormField>
-          <FormField>
-            <label>Last Name</label>
-            <input placeholder='Last Name' />
-          </FormField>
-          <FormField>
-            <label>Email Address</label>
-            <input placeholder='Email Address' />
-          </FormField>
-          <FormField>
-            <label>Phone Number</label>
-            <input placeholder='Phone Number' />
-          </FormField>
-          <Button type='submit' onClick={() => setIsOpen(false)}>Sign Up</Button>
+          <FormGroup>
+            <FormInput 
+              id='form-input-control-first-name' 
+              control={Input} 
+              label='First Name'
+              placeholder='First Name' 
+              required 
+              width={8}
+            />
+            <FormInput 
+              id='form-input-control-last-name'
+              control={Input}
+              label='Last Name' 
+              placeholder='Last Name' 
+              required 
+              width={8}
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormInput
+              id='form-input-control-error-email' 
+              control={Input}
+              label='Email Address' 
+              placeholder='johndoe@email.com' 
+              required 
+              width={8}
+              type='email'
+            />
+            <FormInput
+              id='form-input-control-error-phone-number'
+              control={Input}
+              label='Phone Number' 
+              placeholder='555-123-4567' 
+              required 
+              width={8}
+              type='tel'
+              pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
+            />
+          </FormGroup>
+          <Button type='submit'>Sign Up</Button>
         </Form>
       </ModalContent>
     </Modal>
